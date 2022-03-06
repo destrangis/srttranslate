e635f4977ef7f209ec4817af4b4258bcc4807e69
Javier Llopis <javier@llopis.me>
2022-03-06 12:12:36 +0000

    Add Handler for DeepL

---
+++ deeplhandler.py
@@ -0,0 +1,33 @@
+import deepl
+
+# with open("deepl.key") as fd:
+    # deepl_api_key = fd.read().strip()
+
+
+class DeeplHandler:
+
+    def __init__(self, deepl_api_key):
+        self.transl = deepl.Translator(deepl_api_key)
+        usage = self.transl.get_usage()
+        self.chars = usage.character.count
+        self.limit = usage.character.limit
+
+    def translate(self, inlang, outlang, txt):
+        if inlang == "EN-GB":
+            inlang = "EN"
+        if outlang == "EN":
+            outlang = "EN-GB"
+
+        try:
+            rsp = self.transl.translate_text(txt, source_lang=inlang, target_lang=outlang)
+        except Exception:
+            raise
+        else:
+            self.chars += len(txt)
+        return rsp.text
+
+    def check_quota(self, nchars):
+        # True-- we can translate nchars False-- we can't
+        return (self.chars + nchars) <= self.limit
+
+
